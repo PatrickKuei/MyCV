106a3e8ceb3d584e832e1dc4caa635f8e8a40fd2
PatrickKuei <azureroki@gmail.com>
2020-04-02 18:54:09 +0800

    add portfolioa in vue

---
+++ style.js
@@ -0,0 +1,28 @@
+var apiURL = 'https://api.github.com/users/PatrickKuei/repos'
+var app = new Vue ({
+    el: '#app',
+    data: {
+        texts: '',
+        demoPic: [
+            '<img src="GluttonousSnake.JPG" height="150" width="150">',
+            '<img src="calendar.JPG" height="150" width="150">',
+            '<img src="mycv.JPG" height="150" width="150">',
+            '<img src="XOgame.JPG" height="150" width="150">',
+            '<img src="tab practice.JPG" height="150" width="150">'
+        ]
+    },
+    created: function(){
+        this.fetchData();
+    },
+    methods: {
+        fetchData: function(){
+            var xhr = new XMLHttpRequest();
+            var self = this;
+            xhr.open('GET', apiURL);
+            xhr.onload = function(){
+                self.texts = JSON.parse(xhr.responseText)
+            }
+            xhr.send()
+        }
+    }
+})
